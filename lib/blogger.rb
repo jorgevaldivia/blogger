33342075f26ca3191e2283c112344cc69a9dd0b3
Jorge Valdivia <jorge@valdivia.me>
2011-12-12 22:50:33 -0600

    added authors and published date to posts class

--- lib/blogger.rb
+++ lib/blogger.rb
@@ -321,7 +321,7 @@ module Blogger
       #puts "entry is:\n"
       #puts entry.methods
       @link = entry.links[0]
-      @link = entry.elements["link[@rel='self']"].attributes['href'] 
+      @link = entry.links[0].attributes['href'] 
       @authors = []
       @authors = entry.authors.map {|c| c.name}
       #entry.authors.each do |author|
