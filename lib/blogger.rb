41d7fa804dd267f53f4f8d00f396bff938acff93
Jorge Valdivia <jorge@valdivia.me>
2011-12-12 22:32:21 -0600

    added link to post clas

--- lib/blogger.rb
+++ lib/blogger.rb
@@ -318,6 +318,8 @@ module Blogger
 
       # jvaldivia Added published and author
       @published = entry.published
+      puts "entry is:\n"
+      pp entry
       @link = entry.link
       @authors = []
       @authors = entry.authors.map {|c| c.name}
